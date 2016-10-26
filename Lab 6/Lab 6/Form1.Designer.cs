e801d7ff5569267dff91220eaf6cf18b09ac8837
DmitriyLeaf <dimonkkrukk@gmail.com>
2016-10-27 00:03:48 +0300

    Add files via upload

---
+++ Lab 6/Lab 6/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Lab_6 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(535, 378);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "task1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(616, 378);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "task2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(697, 378);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "task3";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Lab 6/Lab 6/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private int zoom = 10;
+        private int lastX = 0;
+        private int lastY = 0;
+        private double fi = 0.001;
+
+        private void decartCoord() {  //760 360
+            Graphics gr = pictureBox1.CreateGraphics();
+            Pen p = new Pen(Color.Black);
+
+            gr.DrawLine(p, new Point(0, pictureBox1.Height / 2), new Point(pictureBox1.Width, pictureBox1.Height / 2));
+            gr.DrawLine(p, new Point(pictureBox1.Width / 2, 0), new Point(pictureBox1.Width / 2, pictureBox1.Height));
+        }
+        private void polarCoord() {
+            Graphics gr = pictureBox1.CreateGraphics();
+            Pen p = new Pen(Color.Black);
+
+            gr.DrawEllipse(p, pictureBox1.Width / 2 - 1, pictureBox1.Height / 2 - 1, 2, 2);
+            gr.DrawLine(p, new Point(pictureBox1.Width / 2, pictureBox1.Height / 2), new Point(pictureBox1.Width / 2 + 100, pictureBox1.Height / 2));
+            gr.DrawLine(p, new Point(pictureBox1.Width / 2 + 100, pictureBox1.Height / 2), new Point(pictureBox1.Width / 2 + 95, pictureBox1.Height / 2 + 3));
+            gr.DrawLine(p, new Point(pictureBox1.Width / 2 + 100, pictureBox1.Height / 2), new Point(pictureBox1.Width / 2 + 95, pictureBox1.Height / 2 - 3));
+        }
+
+        private void drawPointTaskFirst(double x, double y, Color color) {
+            Graphics gr = pictureBox1.CreateGraphics();
+            Pen p = new Pen(color);
+            int _x = (int)(x * zoom + pictureBox1.Width / 2);
+            int _y = (int)(y * zoom + pictureBox1.Height / 2);
+
+            gr.DrawLine(p, new Point(lastX, lastY), new Point(_x, _y));
+
+            lastY = _y;
+            lastX = _x;
+        }
+
+        private void drawPointTaskSecond(double f, double p, Color color) {
+            double x = p * Math.Cos(f);
+            double y = p * Math.Sin(f);
+            drawPointTaskFirst(x, y, color);
+        }
+
+        private double taskFirstFuncFirst(double x) {
+            double y = 0;
+            y = 4 - Math.Sin(Math.Abs(x));
+            return y;
+        }
+        private double taskFirstFuncSecond(double x) {
+            double y = 0;
+            y = Math.Abs(Math.Sin(x));
+            return y;
+        }
+        private double taskSecondFunc(double f) {
+            double p = 0;
+            p = 400 / (Math.Cos(Math.PI * f) + Math.Cos(Math.E * f) + 4);
+            return p;
+        }
+        private double taskThirdFunc(double t) {
+            double x = 110 * Math.Cos(t) - 10 * Math.Cos(11 * t);
+            double y = 110 * Math.Sin(t) - 10 * Math.Sin(11 * t);
+            return x;
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            // first f
+            double x = -1 * pictureBox1.Width / (2 * zoom);
+            double y = 0;
+
+            zoom = 10;
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.Clear(pictureBox1.BackColor);
+            decartCoord();
+
+            lastX = -1;
+            lastY = (int)taskFirstFuncFirst(lastX) * zoom + pictureBox1.Height / 2;
+
+            for (int i = 0; i < pictureBox1.Width / (fi * zoom); i++) {
+                x += fi;
+                y = taskFirstFuncFirst(x);
+                drawPointTaskFirst(x, y, Color.Red);
+            }
+            // second f
+            x = -1 * pictureBox1.Width / (2 * zoom);
+            y = 0;
+            
+            decartCoord();
+
+            lastX = -1;
+            lastY = (int)taskFirstFuncSecond(lastX) * zoom + pictureBox1.Height / 2;
+
+            for (int i = 0; i < pictureBox1.Width / (fi * zoom); i++) {
+                x += fi;
+                y = taskFirstFuncSecond(x);
+                drawPointTaskFirst(x, y, Color.Green);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.Clear(pictureBox1.BackColor);
+            zoom += 1;
+            button5_Click(sender, e);
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.Clear(pictureBox1.BackColor);
+            zoom -= 1;
+            button5_Click(sender, e);
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            double f = 0;
+            double p = taskSecondFunc(f);
+
+            zoom = 1;
+            fi = 0.001;
+
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.Clear(pictureBox1.BackColor);
+            polarCoord();
+
+            lastX = (int)(p * Math.Cos(f) * zoom + pictureBox1.Width / 2);
+            lastY = (int)(p * Math.Sin(f) * zoom + pictureBox1.Height / 2);
+
+            for (int i = 0; i < 360 / (fi * zoom); i++) {
+                f += fi;
+                p = taskSecondFunc(f);
+                drawPointTaskSecond(f, p, Color.Blue);
+                if (f > 360 * 0.0174532925) break;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            double t = 0;
+            double x = 0;
+            double y = 0;
+
+            zoom = 1;
+            fi = 0.0001;
+
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.Clear(pictureBox1.BackColor);
+            decartCoord();
+
+            lastX = (int)((110 * Math.Cos(t) - 10 * Math.Cos(11 * t)) * zoom + pictureBox1.Width / 2);
+            lastY = (int)((110 * Math.Sin(t) - 10 * Math.Sin(11 * t)) * zoom + pictureBox1.Height / 2);
+
+            for (int i = 0; i < 360 / (fi * zoom); i++) {
+                t += fi;
+                x = 110 * Math.Cos(t) - 10 * Math.Cos(11 * t);
+                y = 110 * Math.Sin(t) - 10 * Math.Sin(11 * t);
+                drawPointTaskFirst(x, y, Color.Green);
+                if (t > 360 * 0.0174532925) break;
+            }
+        }
+    }
+}
+
+
+/*
+    Font myFont = new Font("Arial", 8);
+            gr.DrawString(Convert.ToString(x), myFont, Brushes.Black, new Point(20, wtf));
+            gr.DrawString(Convert.ToString(y), myFont, Brushes.Black, new Point(60, wtf));
+            gr.DrawString(Convert.ToString(_x), myFont, Brushes.Black, new Point(20, wtf + 10));
+            gr.DrawString(Convert.ToString(_y), myFont, Brushes.Black, new Point(60, wtf + 10));
+            wtf += 20;
+*/
+// p = 400/(cos(Pi*f) + cos(E*f) +4);
